0cb21bbcf1c38a95635e137646e54ab5f3a9488d
“Kirill <martynenko13krill@gmail.com>
2023-03-21 13:23:54 +0700

    restore 2ip and abuse

--- abuse.py
+++ abuse.py
@@ -1,6 +1,6 @@
 import requests
 
-def abuse(ip):
+def abuse(ip, api_key):
 	url = 'https://api.abuseipdb.com/api/v2/check'
 	querystring = {
 	    'ipAddress': ip,
@@ -8,10 +8,7 @@ def abuse(ip):
 	}
 	headers = {
 	    'Accept': 'application/json',
-    'Key': 'aed61328696d120ffb33fed3513f4a729edf3d67c9d6dc25c36f2e2107369889b182c28882e73865'
+    'Key': api_key
 	}
 	response = requests.request(method='GET', url=url, headers=headers, params=querystring)
-	print(response.text)
-
-
-abuse('46.56.101.2')
+	return response.text

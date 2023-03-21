4d6477539d135536970a37fd5176b1f9b45db904
shoreco <128378958+shoreco@users.noreply.github.com>
2023-03-22 00:39:34 +0700

    Update abuse.py

--- abuse.py
+++ abuse.py
@@ -11,4 +11,15 @@ def abuse(ip, api_key):
     'Key': api_key
 	}
 	response = requests.request(method='GET', url=url, headers=headers, params=querystring)
-	return response.text
+	res = add_newlines(response.text)
+	res = res[9:-2]
+	return res
+
+def add_newlines(text):
+    new_text = ""
+    for char in text:
+        if char == ",":
+            new_text += "\n"
+        else:
+            new_text += char
+    return new_text

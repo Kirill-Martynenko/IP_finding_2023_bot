6925efdc576709e722bc7264643e069afc0374de
“Kirill <martynenko13krill@gmail.com>
2023-03-24 15:57:38 +0700

    Protected VT

--- virustotal.py
+++ virustotal.py
@@ -1,8 +1,9 @@
 import requests
+import abuse
 
 def getVirusTotal(ip_addr, api_key):
 	url = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
 	params = {'apikey':api_key,'ip':ip_addr}
-	response = requests.get(url, params=params)
+	response = requests.get(url, params=params, timeout=3.5)
 	return response.json()
 
